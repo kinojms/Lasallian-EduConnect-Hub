0a0b840e875d026e742e36f9d2f2e31ea287dd0b
tocinooo <152895495+tocinooo@users.noreply.github.com>
2024-04-05 22:59:45 +0800

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const UserModel = require('../models/userModel');
+const UserModel = require('../models/UserModel');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;

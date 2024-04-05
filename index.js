aafb2c9963aac2249612c457707760a1dd38b08f
tocinooo <152895495+tocinooo@users.noreply.github.com>
2024-04-05 23:37:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,26 +19,20 @@ const app = express();
 // MONGO_URL="mongodb://localhost:27017/MCO";
 //mongoose.connect('mongodb://localhost:27017/MCO');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://tsukino:uElnJfxOaCI5Vspj@mco.qpmivzx.mongodb.net/?retryWrites=true&w=majority&appName=MCO";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://tsukino:<password>@mco.qpmivzx.mongodb.net/?retryWrites=true&w=majority&appName=MCO";
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    await mongoose.disconnect();
   }
 }
 run().catch(console.dir);
